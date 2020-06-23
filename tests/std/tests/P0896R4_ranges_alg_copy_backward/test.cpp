2d24dc9b16935ad186221f97e95f121e5da0ca92
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-23 09:56:09 -0400

    updated algo and added test_bidi_bidi

--- tests/std/tests/P0896R4_ranges_alg_copy_backward/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_copy_backward/test.cpp
@@ -20,22 +20,22 @@ constexpr void smoke_test() {
     int const input[] = {13, 42, 1729};
     { // Validate range overload
         int output[] = {-2, -2, -2};
-        // auto result  = copy_backward(move_only_range{input}, move_only_range{output}.end());
-        // STATIC_ASSERT(same_as<decltype(result),
-        //     copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        // assert(result.in == move_only_range{input}.begin());
-        // assert(result.out == move_only_range{output}.begin());
-        // assert(ranges::equal(output, input));
+        auto result  = copy_backward(move_only_range{input}, move_only_range{output}.end());
+        STATIC_ASSERT(same_as<decltype(result),
+            copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
+        assert(result.in == move_only_range{input}.begin());
+        assert(result.out == move_only_range{output}.begin());
+        assert(ranges::equal(output, input));
     }
     { // Validate iterator + sentinel overload
         int output[] = {-2, -2, -2};
-        // move_only_range wrapped_input{input};
-        // auto result = copy_backward(wrapped_input.begin(), wrapped_input.end(), move_only_range{output}.end());
-        // STATIC_ASSERT(same_as<decltype(result),
-        //     copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        // assert(result.in == wrapped_input.begin());
-        // assert(result.out == move_only_range{output}.begin());
-        // assert(ranges::equal(output, input));
+        move_only_range wrapped_input{input};
+        auto result = copy_backward(wrapped_input.begin(), wrapped_input.end(), move_only_range{output}.end());
+        STATIC_ASSERT(same_as<decltype(result),
+            copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
+        assert(result.in == wrapped_input.begin());
+        assert(result.out == move_only_range{output}.begin());
+        assert(ranges::equal(output, input));
     }
 }
 
@@ -45,11 +45,11 @@ int main() {
 }
 
 struct instantiator {
-    template <class In, class Out>
-    static void call(In&& in = {}, Out out = {}) {
-        (void) ranges::copy_backward(in, ranges::move(out));
-        (void) ranges::copy_backward(ranges::begin(in), ranges::end(in), ranges::move(out));
+    template <class Bidi1, class Bidi2>
+    static void call(Bidi1&& in = {}, Bidi2&& out = {}) {
+        (void) ranges::copy_backward(in, ranges::begin(out));
+        (void) ranges::copy_backward(ranges::begin(in), ranges::end(in), ranges::begin(out));
     }
 };
 
-template void test_in_out<instantiator>();
+template void test_bidi_bidi<instantiator>();
