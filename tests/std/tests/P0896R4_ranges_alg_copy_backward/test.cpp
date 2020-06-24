5061d025784b6f2c81161210ba80c88b4df2ae91
Ahana Mukhopadhyay <t-ahmukh@microsoft.com>
2020-06-24 15:53:52 -0400

    minor updates

--- tests/std/tests/P0896R4_ranges_alg_copy_backward/test.cpp
+++ tests/std/tests/P0896R4_ranges_alg_copy_backward/test.cpp
@@ -19,26 +19,26 @@ constexpr void smoke_test() {
 
     // Validate dangling story needed?
 
-    int const input[] = {13, 42, 1729};
-    { // Validate range overload
-        int output[] = {-2, -2, -2};
-        auto result  = copy_backward(move_only_range{input}, move_only_range{output}.end());
-        STATIC_ASSERT(same_as<decltype(result),
-            copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        assert(result.in == move_only_range{input}.begin());
-        assert(result.out == move_only_range{output}.begin());
-        assert(ranges::equal(output, input));
-    }
-    { // Validate iterator + sentinel overload
-        int output[] = {-2, -2, -2};
-        move_only_range wrapped_input{input};
-        auto result = copy_backward(wrapped_input.begin(), wrapped_input.end(), move_only_range{output}.end());
-        STATIC_ASSERT(same_as<decltype(result),
-            copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
-        assert(result.in == wrapped_input.begin());
-        assert(result.out == move_only_range{output}.begin());
-        assert(ranges::equal(output, input));
-    }
+    // int const input[] = {13, 42, 1729};
+    // { // Validate range overload
+    //     int output[] = {-2, -2, -2};
+    //     auto result  = copy_backward(move_only_range{input}, move_only_range{output}.end());
+    //     STATIC_ASSERT(same_as<decltype(result),
+    //         copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
+    //     assert(result.in == move_only_range{input}.begin());
+    //     assert(result.out == move_only_range{output}.begin());
+    //     assert(ranges::equal(output, input));
+    // }
+    // { // Validate iterator + sentinel overload
+    //     int output[] = {-2, -2, -2};
+    //     move_only_range wrapped_input{input};
+    //     auto result = copy_backward(wrapped_input.begin(), wrapped_input.end(), move_only_range{output}.end());
+    //     STATIC_ASSERT(same_as<decltype(result),
+    //         copy_backward_result<iterator_t<move_only_range<int const>>, iterator_t<move_only_range<int>>>>);
+    //     assert(result.in == wrapped_input.begin());
+    //     assert(result.out == move_only_range{output}.begin());
+    //     assert(ranges::equal(output, input));
+    // }
 }
 
 int main() {
